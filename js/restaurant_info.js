5c1c3570ead687ff2fb30c5bebb4f9c9bcf69d16
brunoannunciato <brunobannunciato@gmail.com>
2019-03-16 17:29:18 -0300

    add token do map

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: '<your MAPBOX API KEY HERE>',
+        mapboxToken: 'pk.eyJ1IjoiYnJ1bm9hbm51bmNpYXRvIiwiYSI6ImNqdDNkb291dTJqaDQ0NG9kOGpkb2djZW0ifQ.eWxAzu-C8LYHzeu-7fJR5w',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
